73b96e9e2d827b3f5bf652d5ed0c43fc045cb207
rkaraujo <rkaraujo@gmail.com>
2024-04-22 23:11:08 -0300

    Modified text fields

--- rka_jam_app/models.py
+++ rka_jam_app/models.py
@@ -6,10 +6,10 @@ class JobApplication(models.Model):
     job_title = models.CharField(max_length=100, help_text='Job title')
     job_url = models.URLField(null=True, help_text='The url for the job ad')
     recruiter = models.CharField(null=True, max_length=100, help_text='Recruiter name')
-    recruiter_contacts = models.TextField(null=True, help_text='Recruiter contacts')
+    recruiter_contacts = models.TextField(null=True, blank=True, help_text='Recruiter contacts')
     job_description = models.TextField(help_text='The job description')
     date_applied = models.DateField(auto_now_add=True, help_text='The date I applied')
-    notes = models.TextField(help_text='Any notes relevant to the process')
+    notes = models.TextField(null=True, blank=True, help_text='Any notes relevant to the process')
     rejected = models.BooleanField(default=False, help_text='If I was rejected in this process')
 
     def __str__(self):
